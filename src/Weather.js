29684e7306450d5661481670174a0541c00bd72a
march-dave <ysangy@gmail.com>
2019-08-29 18:50:11 -0400

    up to date

--- src/Weather.js
+++ src/Weather.js
@@ -47,9 +47,8 @@ class Weather extends Component {
         let result = [];
         let res_city;
 
-        console.log(data.city.name);
-
-        console.log(data);
+        // console.log(data.city.name);
+        // console.log(data);
       });
 
     e.preventDefault();
